b974e0bd36622168720abe844de3e3a086cc68a1
bradlet <bradlet2@pdx.edu>
2019-10-21 21:37:49 -0700

    Integrated with Database & changed to ScrollView (read more)
    ISSUE: Need to find a way to fix a footer of sorts, like in
    CreateAuditPage, such that the back and exit button are present at all
    times.
    
    Included connecting to the database, inheriting from Screen so that the
    ViewAuditsPage class can work with the screen manager when it gets to be
    time to make it work with the home page. I don't have time to get to it
    tonight.
    
    Also made a scrollview to contain the gridlayout. If it had been left as
    a gridlayout alone, only a fixed number of items could be shown in the
    list. Now, it can update to contain as many as necessary.
    
    Also changed some naming to add clarity in separating the terms
    "Audit" and "AuditTemplate".

--- cilantro_audit/view_audits_page.py
+++ cilantro_audit/view_audits_page.py
@@ -1,47 +1,66 @@
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
+from kivy.uix.scrollview import ScrollView
+from audit_template import AuditTemplate
+from create_audit_page import ConfirmationPop
+from mongoengine import connect
 
 kivy.require('1.11.1')
 
+connect("toost")
 
-class ViewAuditsPage(App):
+class ViewAuditsPage(Screen, App):
 
     def build(self):
         # Initialize page information and layout
         self.title = 'CilantroAudit - View Audits Page'
-        page_layout = GridLayout(cols=1)
+        page_layout = GridLayout(cols=1, spacing=5, size_hint_y=None)
+        # https://kivy.org/doc/stable/api-kivy.uix.scrollview.html?highlight=view#
+        page_layout.bind(minimum_height=page_layout.setter('height'))
 
-        # Retrieve audits from database
-        audits_list = ['Audit 1', 'Audit 2', 'Audit 3', 'Audit 4', 'Audit 5']
+        # Get a list of strings - the AuditTemplate's titles - from the database
+        audits_list = list(map(lambda obj: obj.title, AuditTemplate.objects.only('title')))
 
-        # Add audit buttons to the page
-        for audit in audits_list:
-            page_layout.add_widget(Button(text=audit,
-                                          font_size=20,
+        # Add AuditTemplate buttons to the page
+        for title in audits_list:
+            page_layout.add_widget(Button(text=title,
+                                          font_size=30,
+                                          size_hint_y=None,
                                           on_press=self.go_to_page))
+
+        # https://kivy.org/doc/stable/api-kivy.uix.scrollview.html?highlight=view#
+        root = ScrollView(size_hint=(1,None),
+                          size=(Window.width, Window.height),
+                          do_scroll_x=False)
 
         # Add bottom bar to the page
         bottom_layout = GridLayout(cols=2)
         bottom_layout.add_widget(Button(text='Return To Homepage',
                                         font_size=20,
-                                        on_press=self.go_to_page))
+                                        on_press=self.back(root.manager)))
         bottom_layout.add_widget(Button(text='Exit',
                                         font_size=20,
                                         size_hint=(.4, .2),
-                                        on_press=self.exit))
+                                        on_press=self.exit()))
         page_layout.add_widget(bottom_layout)
 
-        return page_layout
+        root.add_widget(page_layout)
+        return root
 
-    @staticmethod
-    def go_to_page(self):
-        return
+    def back(self, manager):
+        show = ConfirmationPop()
+        show.manager = manager
+        show.open()
 
-    @staticmethod
     def exit(self):
         exit(1)
+
+    def go_to_page(self):
+        return
 
 
 if __name__ == '__main__':
