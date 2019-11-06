a3778a782202a3fdcf92239fc63edf2b48e45fb5
seanlesch <38013435+seanlesch@users.noreply.github.com>
2019-11-06 08:27:08 -0800

    Pushed work done with Steven towards integrating.

--- cilantro_audit/view_audit_templates.py
+++ cilantro_audit/view_audit_templates.py
@@ -6,9 +6,10 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
-from cilantro_audit.constants import KIVY_REQUIRED_VERSION
+from cilantro_audit.constants import KIVY_REQUIRED_VERSION, CREATE_COMPLETED_AUDIT_PAGE
 from cilantro_audit.constants import PROD_DB
 from cilantro_audit.audit_template import AuditTemplate
+from cilantro_audit.templates.audit_button import AuditButtonTemplate
 
 # Required Version
 kivy.require(KIVY_REQUIRED_VERSION)
@@ -16,15 +17,12 @@ kivy.require(KIVY_REQUIRED_VERSION)
 Builder.load_file("./widgets/view_audit_templates.kv")
 
 
-# Will implement opening of an audit to fill out.
-class AuditButton(Button):
-    pass
-
 
 # Handles the retrieval of audit templates for the auditor screens.
 class ViewAuditTemplates(Screen):
     # Holds the list of titles retrieved from the database.
     templates_list = ObjectProperty()
+    root = ObjectProperty()
     connect(PROD_DB)
 
     # Constructor utilizes the only method to retrieve audits for use in the associated .kv file.
@@ -37,8 +35,11 @@ class ViewAuditTemplates(Screen):
     def get_audit_templates(self):
         titles = list(map(lambda template: template.title, AuditTemplate.objects().only('title')))
         for title in titles:
-            self.templates_list.add_widget(AuditButton(text=title))
+            self.templates_list.add_widget(Button(text=title, on_press=self.load_page(title=title)))
 
+
+    def load_page(self, title):
+        self.root.manager.get_screen(CREATE_COMPLETED_AUDIT_PAGE).populate_audit(target=title)
 
 class TestApp(App):
     def build(self):
